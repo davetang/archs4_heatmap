04551fc8fc0f16194766f1e77f1c77ed340b130d
Dave Tang <davetingpongtang@gmail.com>
2024-08-09 08:32:18 +0900

    Column clustering is off by default; use --cluster-cols to perform clustering

--- script/heatmap.R
+++ script/heatmap.R
@@ -4,7 +4,7 @@
 # Year 2023
 #
 
-script_ver <- '0.0.2'
+script_ver <- '0.0.3'
 
 suppressPackageStartupMessages(library(optparse))
 suppressPackageStartupMessages(library(pheatmap))
@@ -16,9 +16,11 @@ option_list <- list(
    make_option(c("-v", "--version"), action = "store_true", help =
                "Show script version and exit"),
    make_option(c("-o", "--out"), default = "heatmap.png",
-               help = "Heatmap file name (default = %default)"),
+               help = "Output heatmap file name (default = %default)"),
    make_option(c("-m", "--matrix"), default = "matrix.csv",
-               help = "Matrix file name (default = %default)")
+               help = "Output matrix file name (default = %default)"),
+   make_option(c("-c", "--cluster_cols"), action = "store_true", default = FALSE,
+               help = "Cluster columns (default = %default)")
 )
 
 # create your own usage
@@ -70,6 +72,8 @@ id_split$tissue <- sapply(id_split$tissue, cap_first)
 
 my_df <- cbind(my_df, id_split)
 my_df <- my_df[order(my_df$gene, my_df$system), ]
+my_df$system <- factor(my_df$system, levels = unique(my_df$system))
+my_df$organ <- factor(my_df$organ, levels = unique(my_df$organ))
 my_df$tissue <- factor(my_df$tissue, levels = unique(my_df$tissue))
 
 my_df |>
@@ -82,13 +86,24 @@ my_order <- colnames(my_mat)
 write.csv(my_mat, file = opt$options$matrix, quote = FALSE)
 
 my_df |>
-  dplyr::select(system, tissue) |>
+  dplyr::select(system, organ, tissue) |>
   dplyr::distinct() |>
   dplyr::arrange(match(tissue, my_order)) |>
   dplyr::select(-tissue) -> sample_anno
+
 row.names(sample_anno) <- my_order
 
-p <- pheatmap(my_mat, annotation_col = sample_anno, silent = TRUE)
+# used to introduce gaps in the heatmap if no clustering
+sample_anno$system |>
+  as.numeric() |>
+  diff() -> my_breaks
+my_breaks <- which(my_breaks == 1)
+
+if(opt$options$cluster_cols == TRUE){
+   p <- pheatmap(my_mat, annotation_col = sample_anno, silent = TRUE)
+} else {
+   p <- pheatmap(my_mat, annotation_col = sample_anno, cluster_cols = FALSE, gaps_col = my_breaks, silent = TRUE)
+}
 
 my_width  <- 700 * sqrt(nrow(my_mat))
 my_height <- 400 * sqrt(nrow(my_mat))
